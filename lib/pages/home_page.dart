4f771998057b4f550397a0dc5a68e13380613b36
Kunal Gurjar <kunalgurjarkg786@gmail.com>
2022-09-11 17:03:04 +0530

    Day 4 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  final int date = 30;
-  final String name = "gurjar";
-
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
         child: Container(
-          child: Text("Hello $name"),
+          child: Text("Hello Gurjar"),
         ),
       ),
+      drawer: Drawer(),
     );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              width: 500,
+              height: 200,
+              fit: BoxFit.fill,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Hi Gurjar");
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
